ad10f5bb417c45964b03d27554d8a341c2399a4d
Pasan24 <pramudithapasan076@gmail.com>
2024-10-31 06:27:23 +0530

    Created Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/header/header.jsx"
 import HomePage from "./pages/client-page/homePage.jsx"
 import AdminPage from "./pages/adminPage/admin.jsx"
+import TestComponent from "./components/test/test.jsx"
+import LoginPage from "./pages/login/login.jsx"
 
 
 
@@ -15,6 +17,8 @@ function App() {
         <Route path="/*" element={<HomePage/>} />
         
         <Route path="/admin/*" element= {<AdminPage/>} />
+
+        <Route path="/login" element= {<LoginPage/>} />
 
       
    
