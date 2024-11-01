90fdc14c1616949935ebecf2641bec7063b2d5e8
Pasan24 <pramudithapasan076@gmail.com>
2024-11-01 10:31:14 +0530

    created env file

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./login.css";
 
 export default function LoginPage() {
-
     
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="w-full h-[100vh] bg-blue-600 pic-bg  flex justify-center items-center">
       <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col items-center justify-center relative">
-        <h1 className=" text-4xl text-black text-center p-[15px] absolute top-[40px] text-center">
-            Login
+        <h1 className=" text-4xl text-white text-center p-[15px] absolute top-[40px] text-center">
+          Login
         </h1>
 
         <input
@@ -16,18 +18,31 @@ export default function LoginPage() {
           placeholder="Enter your Email  Address "
           className="w-[80%] border text-white bg-[#00000000] 
                      border-[2px] placeholder:text-white border-white h-[50px] px-[5px] mb-[5px]"
-        />
+         defaultvalue={email}
+         onChange={
+            (e)=>{
+                setEmail(e.target.value)
+
+            }
+         } />
 
         <input
           type="password"
           placeholder="Enter your Password "
           className="w-[80%] border text-white bg-[#00000000] 
                      border-[2px] placeholder:text-white border-white h-[50px] px-[5px] mb-[5px]"
-        />
+            defaultvalue={password}  onChange={
+                (e)=>{
+                    setPassword(e.target.value)
 
-        <button  className=" w-[80%] absolute bottom-[40px] text-white h-[50px] bg-red-500 " > Login</button>
+                }
+            }
+            />
 
-
+        <button className=" w-[80%] absolute bottom-[40px] text-white h-[50px] bg-red-500 ">
+         
+          Login
+        </button>
       </div>
     </div>
   );
