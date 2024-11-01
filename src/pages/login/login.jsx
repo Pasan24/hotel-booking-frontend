99491d473d40262cc38f501c83f7c40b0e47323a
Pasan24 <pramudithapasan076@gmail.com>
2024-11-01 10:32:06 +0530

    created env file

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,32 @@
 import { useState } from "react";
 import "./login.css";
+import axios from "axios";
 
 export default function LoginPage() {
-    
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleLogin() {
+
+    axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login", {
+
+        email: email,
+        password: password,
+
+      })
+      
+      .then((res) => {
+        console.log(res.data)
+        localStorage.setItem("token",res.data.token)
+
+        const token = localStorage.getItem("token")
+        coonsole.log(token)
+
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
 
   return (
     <div className="w-full h-[100vh] bg-blue-600 pic-bg  flex justify-center items-center">
@@ -18,29 +40,27 @@ export default function LoginPage() {
           placeholder="Enter your Email  Address "
           className="w-[80%] border text-white bg-[#00000000] 
                      border-[2px] placeholder:text-white border-white h-[50px] px-[5px] mb-[5px]"
-         defaultvalue={email}
-         onChange={
-            (e)=>{
-                setEmail(e.target.value)
-
-            }
-         } />
+          defaultValue={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
 
         <input
           type="password"
           placeholder="Enter your Password "
           className="w-[80%] border text-white bg-[#00000000] 
                      border-[2px] placeholder:text-white border-white h-[50px] px-[5px] mb-[5px]"
-            defaultvalue={password}  onChange={
-                (e)=>{
-                    setPassword(e.target.value)
+          defaultValue={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-                }
-            }
-            />
-
-        <button className=" w-[80%] absolute bottom-[40px] text-white h-[50px] bg-red-500 ">
-         
+        <button
+          className=" w-[80%] absolute bottom-[40px] text-white h-[50px] bg-red-500 "
+          onClick={handleLogin}
+        >
           Login
         </button>
       </div>
